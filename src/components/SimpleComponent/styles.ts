a073f1a318ab87bb994b1e129ecf74c0a351dfd2
andrevmarkelov <andrev_markelov@mail.ru>
2021-08-11 18:11:53 +0300

    added table layout

--- src/components/SimpleComponent/styles.ts
+++ src/components/SimpleComponent/styles.ts
@@ -4,4 +4,52 @@ export const useStyles = makeStyles({
     someClass: {
         padding: '10px',
     },
+    wrapper: {
+        width: '1200px',
+        margin: '0 auto',
+        padding: '50px 0px',
+        fontFamily: 'Montserrat',
+    },
+    tableHeader: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+    },
+    tableBody: {
+        width: '100%',
+    },
+    itemTable: {
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        borderBottom: '1px solid #c1c1c1',
+        padding: '20px 0px',
+    },
+    itemTableHeader: {
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
+    itemTableBody: {
+        width: '100%',
+    },
+    orderPrice: {
+        color: '#00cd5e',
+        fontWeight: 'bold',
+    },
+    orderLink: {
+        color: '#026dc0',
+        fontSize: '17px',
+        textDecoration: 'none',
+    },
+    orderDescription: {
+        fontSize: '14px',
+    },
+    orderDate: {
+        fontSize: '11px',
+        fontWeight: 'bold',
+        margin: '0',
+        marginTop: '25px',
+    },
 });
--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -1,8 +1,46 @@
-import {Container} from '@material-ui/core';
+import {Button} from '@material-ui/core';
 import React from 'react';
+import {useStyles} from '../SimpleComponent/styles';
 
 export const Table: React.FC = () => {
+    const classes = useStyles();
+
+    const currDate = new Date().toLocaleDateString();
+
     return (
-        <Container maxWidth="sm">Table</Container>
+        <div className={classes.wrapper}>
+            <div className={classes.tableHeader}>
+                <h1>Вся работа</h1>
+                <Button variant="contained" color="secondary">Разместить заказ</Button>
+            </div>
+            <div className={classes.tableBody}>
+                <div className={classes.itemTable}>
+                    <div className={classes.itemTableHeader}>
+                        <a href="/" className={classes.orderLink}>Разработка приложения для логиста и курьеров</a>
+                        <span className={classes.orderPrice}>150$</span>
+                    </div>
+                    <div className={classes.itemTableBody}>
+                        <p className={classes.orderDescription}>
+                            Необходимо создать приложение для курьеров и логиста, более подробно во вложении.
+                        </p>
+                        <p className={classes.orderDate}>{currDate}</p>
+                    </div>
+                </div>
+                {/* temporary */}
+                <div className={classes.itemTable}>
+                    <div className={classes.itemTableHeader}>
+                        <a href="/" className={classes.orderLink}>Создание серверной расчетной программы</a>
+                        <span className={classes.orderPrice}>220$</span>
+                    </div>
+                    <div className={classes.itemTableBody}>
+                        <p className={classes.orderDescription}>
+                        В рамках реализации нового проекта компании Danfoss, стоит задача реализации сервера
+                        для проведения ряда расчетов для подбора оборудования.
+                        </p>
+                        <p className={classes.orderDate}>{currDate}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
