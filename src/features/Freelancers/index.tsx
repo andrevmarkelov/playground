b1a98619f76edc6a23df03d1c322ad05d45498e3
andrevmarkelov <andrev_markelov@mail.ru>
2021-08-21 16:35:20 +0300

    Added changes to the freelancers page

--- src/features/Freelancers/index.tsx
+++ src/features/Freelancers/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
 import {useStyles} from './styles';
+import {Freelancer} from '../../store/Freelancers/types';
 
-export const Freelancers: React.FC = () => {
+export const Freelancers: React.FC<Freelancer> = ({id, email, username, phone, rating, about, registered}) => {
     const classes = useStyles();
 
     return (
-        <div className={classes.someClass}>Freelancers</div>
+        <div className={classes.someClass}>
+            <p>Freelancers</p>
+            <p>{id}</p>
+            <p>{email}</p>
+            <p>{username}</p>
+            <p>{phone}</p>
+            <p>{rating}</p>
+            <p>{about}</p>
+            <p>{registered}</p>
+        </div>
     );
 };
